afa6624284d2869822774527100791d8b6a96082
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-05-15 12:10:50 +1000

    changeBarColors function no longer takes a input

--- helper/changeBarColours.js
+++ helper/changeBarColours.js
@@ -1,6 +1,7 @@
 import { elements } from "../index.js";
+import { color } from "../index.js";
 
-export function changeBarColours(color) {
+export function changeBarColours() {
     elements.forEach(element => {
         element.style.backgroundColor = color;
     })
