5bbf225de676ac4c8828a43b60c25b887a8810b0
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-05-15 12:11:14 +1000

    change bar heights no longer takes in any inputs

--- helper/changeBarHeights.js
+++ helper/changeBarHeights.js
@@ -1,8 +1,10 @@
 import { elements } from "../index.js";
+import { numElements } from "../index.js";
+import { nums } from "../index.js";
 
-export function changeBarHeights(nums) {
+export function changeBarHeights() {
     elements.forEach(element => {
-        let percentage = (nums[parseInt(element.id)]/100)*100;
+        let percentage = (nums[parseInt(element.id)]/numElements)*100;
         element.style.height = `${percentage}%`;
     })
 }
