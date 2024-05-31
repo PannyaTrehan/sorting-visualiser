c6e8b19b1ac7a6f1810827e151bcec0930c00d6f
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-06-01 03:05:13 +1000

    created amd index.js file for algorithm and helper functions

---
+++ algorithms/index.js
@@ -0,0 +1,2 @@
+export { mergeSort } from "./mergeSort.js";
+export { quickSort } from "./quickSort.js";
---
+++ helper/index.js
@@ -0,0 +1,5 @@
+export { sleep } from './sleep.js';
+export { changeBarHeights } from './changeBarHeights.js';
+export { changeBarColours } from './changeBarColours.js';
+export { finalVisualise } from './endAnimation.js';
+export { shuffleArray } from './shuffleArray.js';
